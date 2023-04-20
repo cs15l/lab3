1690c68f362a590251a4193c0f1bbfc33e5dcb73
cs15l <bmk001@ucsd.edu>
2023-04-20 09:30:51 -0700

    commit1

--- ArrayTests.java
+++ ArrayTests.java
@@ -4,15 +4,23 @@ import org.junit.*;
 public class ArrayTests {
 	@Test 
 	public void testReverseInPlace() {
-    int[] input1 = { 3 };
+    int[] input1 = {1,2,3};
     ArrayExamples.reverseInPlace(input1);
-    assertArrayEquals(new int[]{ 3 }, input1);
+    assertArrayEquals(new int[]{3,2,1}, input1);
 	}
 
 
   @Test
   public void testReversed() {
-    int[] input1 = { };
-    assertArrayEquals(new int[]{ }, ArrayExamples.reversed(input1));
+    int[] input1 = {1,2,3};
+    assertArrayEquals(new int[]{3,2,1}, ArrayExamples.reversed(input1));
+  }
+
+  @Test
+  public void testAverageWithoutLowest(){
+    double[] input1 = {3,3};
+    assertEquals(3,ArrayExamples.averageWithoutLowest(input1),0.00000000000000000000001);
   }
 }
+
+
---
+++ ListTests.java
@@ -0,0 +1,46 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+import java.util.ArrayList;
+
+
+public class ListTests {
+    @Test
+    public void testFilter(){
+        ArrayList<String> al = new ArrayList<String>();
+        al.add("Brandon");
+        al.add("Ziad");
+        al.add("Dhruv");
+        al.add("Colby");
+        al.add("Shashwat");
+        
+        assertArrayEquals(new String[]{"Brandon","Shashwat"},ListExamples.filter(al,new LargeString()).toArray());
+        
+    }
+    /* 
+    @Test
+    public void testMerge(){
+        ArrayList<String> a = new ArrayList<String>();
+        ArrayList<String> b = new ArrayList<String>();
+        b.add("a");
+        a.add("a");
+        b.add("b");
+        a.add("b");
+        b.add("b");
+        a.add("c");
+        b.add("e");
+        b.add("e");
+        b.add("e");
+        a.add("d");
+        assertArrayEquals(new String[]{"a","a","b","b","b","c","d","e","e","e"},ListExamples.merge(a,b).toArray());
+    }
+    */
+}
+
+
+
+
+class LargeString implements StringChecker{
+    public boolean checkString(String s){
+      return s.length()>5;
+    }
+}
